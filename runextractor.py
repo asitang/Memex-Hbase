79dc68cff80488fa7f0919bbb28818e39ded2cfd
asitang <asitang@gmail.com>
2016-09-19 15:15:53 -0700

    fixed unicode bug

--- runextractor.py
+++ runextractor.py
@@ -6,7 +6,7 @@ def extract(folderin,folderout,filenumber):
     output = subprocess.check_output(
         'CLASSPATH=".:/mnt/Training\ Tesseract/TesseractOCRConfig.properties" '
         +'java -Dtika.xml.content -jar /deploy/parser-indexer/parser-indexer-1.0-SNAPSHOT.jar postdump'
-         +' -list '+folderin+'/'+filenumber+' -out '+folderout+'/'+filenumber+'.json',
+         +' -list '+folderin+'/'+filenumber+' -out '+folderout+'/'+filenumber,
         shell=True,
         )
     #print output here
